5aef3c20027f5d5b96b6bb2964d1c879d39a7a47
Wes Mason <wes@1stvamp.org>
2013-05-09 12:29:40 +0100

    Replace new Function generated template callback with a closure

--- js/datepicker.js
+++ js/datepicker.js
@@ -1,5 +1,6 @@
 /**
- * DatePicker 1.0.0
+ * ex: set tabstop=2 shiftwidth=2 expandtab:
+ * DatePicker 1.0.1-serverdensity1
  * 
  * A jQuery-based DatePicker that provides an easy way of creating both single
  * and multi-viewed calendars capable of accepting single, range, and multiple
@@ -999,26 +1000,18 @@
     // load the template - and be sure to cache the result.
     var fn = !/\W/.test(str) ?
       cache[str] = cache[str] ||
-        tmpl(document.getElementById(str).innerHTML) :
-     
+      tmpl(document.getElementById(str).innerHTML) :
+
       // Generate a reusable function that will serve as a template
       // generator (and which will be cached).
-      new Function("obj",
-        "var p=[],print=function(){p.push.apply(p,arguments);};" +
-       
-        // Introduce the data as local variables using with(){}
-        "with(obj){p.push('" +
-       
-        // Convert the template into pure JavaScript
-        str
-          .replace(/[\r\t\n]/g, " ")
-          .split("<%").join("\t")
-          .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-          .replace(/\t=(.*?)%>/g, "',$1,'")
-          .split("\t").join("');")
-          .split("%>").join("p.push('")
-          .split("\r").join("\\'")
-      + "');}return p.join('');");
+      function(data) {
+        for (key in data) {
+          if (data.hasOwnProperty(key)) {
+            str = str.replace('<%=' + key + '=>', data[key]);
+          }
+        }
+        return str;
+      };
    
     // Provide some basic currying to the user
     return data ? fn( data ) : fn;
